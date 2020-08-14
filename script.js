bb743f8ed0a397fa26ee37146a060983ec1d9402
like4628 <37355283+like4628@users.noreply.github.com>
2020-08-14 23:14:44 +0800

    m

--- script.js
+++ script.js
@@ -16,28 +16,28 @@ $('document').ready(function () {
   //wow js animation
   new WOW().init();
 
+
+
+
+
+  //滾動進度條
+  (function () {
+    let pageHeight = document.body.scrollHeight || document.documentElement.scrollHeight; // 页面总高度
+    let windowHeight = document.documentElement.clientHeight || document.body.clientHeight; // 浏览器视口高度
+    let scrollAvail = pageHeight - windowHeight; // 可滚动的高度
+    // console.log('可滚动的高度：', scrollAvail);
+    window.onscroll = function () {
+      let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;  // 获取滚动条的高度
+      console.log('滚动条的高度：', scrollTop);
+      document.querySelector('.progress').style.width = (scrollTop / scrollAvail) * 100 + '%';    // 计算占比
+    };
+  }
+    ());
+
   //Aos js animation
   AOS.init({
     duration: 1000,
   })
-
-
-
-
-    //滾動進度條
-    (function () {
-      let pageHeight = document.body.scrollHeight || document.documentElement.scrollHeight; // 页面总高度
-      let windowHeight = document.documentElement.clientHeight || document.body.clientHeight; // 浏览器视口高度
-      let scrollAvail = pageHeight - windowHeight; // 可滚动的高度
-      console.log('可滚动的高度：', scrollAvail);
-      window.onscroll = function () {
-        let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;  // 获取滚动条的高度
-        console.log('滚动条的高度：', scrollTop);
-        document.querySelector('.progress').style.width = (scrollTop / scrollAvail) * 100 + '%';    // 计算占比
-      };
-    }
-      ());
-
 
 
 
