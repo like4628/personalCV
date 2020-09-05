9b370812fac375c855cd7d419790d8d4af4097e9
like4628 <37355283+like4628@users.noreply.github.com>
2020-09-05 11:26:06 +0800

    update effect and text

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-$('document').ready(function () {
-
-//loading
+$("document").ready(function () {
+  //loading
   setTimeout(() => {
-    $('.preload').fadeOut(2000)
+    $(".preload").fadeOut(2000);
   }, 1500);
-  
-  $('.loading').delay(1500).fadeOut(200)
 
+  $(".loading").delay(1500).fadeOut(200);
 
-//navBar show 
+  //navBar show
   $(window).on("scroll", function () {
     var scrollDistance = $(window).scrollTop();
     var $header = $(".jsHeader");
@@ -16,134 +14,146 @@ $('document').ready(function () {
     if (scrollDistance > 50) {
       $header.addClass("headerTransparent");
     } else {
-      $header.removeClass("headerTransparent")
+      $header.removeClass("headerTransparent");
     }
-  })
+  });
 
   //wow js animation
   new WOW().init();
 
-
-
-
-
   //滾動進度條
   (function () {
-      let pageHeight = document.body.scrollHeight || document.documentElement.scrollHeight; // 頁面高度
-      let windowHeight = document.documentElement.clientHeight || document.body.clientHeight; // 瀏覽器高度
-      let scrollAvail = pageHeight - windowHeight; // 滾動高度
-      console.log('可滾動的高度：', scrollAvail);
-      window.onscroll = function () {
-        let scrollTop = document.documentElement.scrollTop || document.body.scrollTop; // 滾動條高度
-        console.log('滾動條高度', scrollTop);
-        document.querySelector('.progress').style.width = (scrollTop / scrollAvail) * 100 + '%'; // 進度條比例
-      };
-    }
-    ());
+    let pageHeight =
+      document.body.scrollHeight || document.documentElement.scrollHeight; // 頁面高度
+    let windowHeight =
+      document.documentElement.clientHeight || document.body.clientHeight; // 瀏覽器高度
+    let scrollAvail = pageHeight - windowHeight; // 滾動高度
+    console.log("可滾動的高度：", scrollAvail);
+    window.onscroll = function () {
+      let scrollTop =
+        document.documentElement.scrollTop || document.body.scrollTop; // 滾動條高度
+      console.log("滾動條高度", scrollTop);
+      document.querySelector(".progress").style.width =
+        (scrollTop / scrollAvail) * 100 + "%"; // 進度條比例
+    };
+  })();
 
   //Aos js animation
   AOS.init({
     duration: 1000,
-  })
+  });
 
-
-
-
-
-  //fancyBox 3.0 如果是3.1以上的版本不能使用position:relative 
+  //fancyBox 3.0 如果是3.1以上的版本不能使用position:relative
   //要去修改裡面為position:fixed
   $("[data-fancybox]").fancybox({
-    loop: true
+    loop: true,
   });
 
   //scrolltop網頁緩慢滑動
 
-  $('#btn').click(function () {
-    $('html,body').animate({
-      scrollTop: $('#contact_bottom').offset().top
-    }, 2000);
+  $("#btn").click(function () {
+    $("html,body").animate(
+      {
+        scrollTop: $("#contact_bottom").offset().top,
+      },
+      2000
+    );
   }); //代表一個完整的執行區塊
 
-  $('#home').click(function () {
-    $('html,body').animate({
-      scrollTop: $('#topPage').offset().top
-    }, 500);
+  $("#home").click(function () {
+    $("html,body").animate(
+      {
+        scrollTop: $("#topPage").offset().top,
+      },
+      500
+    );
   }); //代表一個完整的執行區塊
 
-  $('#skill').click(function () {
-    $('html,body').animate({
-      scrollTop: $('#skillPage').offset().top
-    }, 500);
+  $("#skill").click(function () {
+    $("html,body").animate(
+      {
+        scrollTop: $("#skillPage").offset().top,
+      },
+      500
+    );
   }); //代表一個完整的執行區塊
 
-  $('#experience').click(function () {
-    $('html,body').animate({
-      scrollTop: $('#ExperiencePage').offset().top
-    }, 500);
+  $("#experience").click(function () {
+    $("html,body").animate(
+      {
+        scrollTop: $("#ExperiencePage").offset().top,
+      },
+      500
+    );
   }); //代表一個完整的執行區塊
 
-  $('#myworks').click(function () {
-    $('html,body').animate({
-      scrollTop: $('#WorksPage').offset().top
-    }, 500);
+  $("#myworks").click(function () {
+    $("html,body").animate(
+      {
+        scrollTop: $("#WorksPage").offset().top,
+      },
+      500
+    );
   }); //代表一個完整的執行區塊
 
-  $('#email').click(function () {
-    $('html,body').animate({
-      scrollTop: $('#emailPage').offset().top
-    }, 500);
+  $("#email").click(function () {
+    $("html,body").animate(
+      {
+        scrollTop: $("#emailPage").offset().top,
+      },
+      500
+    );
   }); //代表一個完整的執行區塊
-
 
   //聯絡
-  $('.sendBtn').click(function (event) {
+  $(".sendBtn").click(function (event) {
     event.preventDefault();
-    console.log('cccc')
+    console.log("cccc");
     //sweetAlert2
     Swal.fire({
-      icon: 'success',
-      title: '請聯絡下面信箱',
-      text: 't102332026@ntut.org.tw',
+      icon: "success",
+      title: "請聯絡下面信箱",
+      text: "t102332026@ntut.org.tw",
       // footer: '<label>Email:  t102332026@ntut.org.tw</label>'
+    });
+  });
 
-    })
-  })
+  //圖片過度
+  new hoverEffect({
+    parent: document.querySelector(".distortionCounter"),
+    intensity: 0.2,
+    speedIn: 1,
+    image1: "./asset/worksCounter.PNG",
+    image2: "./asset/worksCounter2.PNG",
+    displacementImage: "./asset/distortion.png",
+    imagesRatio: 1080 / 1920,
+  });
+  new hoverEffect({
+    parent: document.querySelector(".distortionEsports"),
+    intensity: 0.2,
+    speedIn: 1,
+    image1: "./asset/esprts.PNG",
+    image2: "./asset/esprts2.PNG",
+    displacementImage: "./asset/distortion.png",
+    imagesRatio: 1080 / 1920,
+  });
 
-
-//圖片過度
-new hoverEffect({
-  parent: document.querySelector('.distortionCounter'),
-  intensity: 0.2,
-  image1: './asset/worksCounter.PNG',
-  image2: './asset/worksCounter2.PNG',
-  displacementImage: './asset/distortion.png',
-  imagesRatio: 1080 /1920
+  new hoverEffect({
+    parent: document.querySelector(".distortionCar"),
+    intensity: 0.2,
+    speedIn: 1,
+    image1: "./asset/superCar.PNG",
+    image2: "./asset/superCar2.PNG",
+    displacementImage: "./asset/distortion.png",
+    imagesRatio: 1080 / 1920,
+  });
+  new hoverEffect({
+    parent: document.querySelector(".distortionAE"),
+    intensity: 0.2,
+    speedIn: 1,
+    image1: "./asset/video.PNG",
+    image2: "./asset/youtube.png",
+    displacementImage: "./asset/distortion.png",
+    imagesRatio: 1080 / 1920,
+  });
 });
-new hoverEffect({
-  parent: document.querySelector('.distortionEsports'),
-  intensity: 0.2,
-  image1: './asset/esprts.PNG',
-  image2: './asset/esprts2.PNG',
-  displacementImage: './asset/distortion.png',
-  imagesRatio: 1080 /1920
-});
-
-new hoverEffect({
-  parent: document.querySelector('.distortionCar'),
-  intensity: 0.2,
-  image1: './asset/superCar.PNG',
-  image2: './asset/superCar2.PNG',
-  displacementImage: './asset/distortion.png',
-  imagesRatio: 1080 /1920
-});
-new hoverEffect({
-  parent: document.querySelector('.distortionAE'),
-  intensity: 0.2,
-  image1: './asset/video.PNG',
-  image2: './asset/youtube.png',
-  displacementImage: './asset/distortion.png',
-  imagesRatio: 1080 /1920
-});
-
-
-})
